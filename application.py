4a504c5d3278043e3199a4d669730f321351a5ce
Shravya <geediguntashravya@msitprogram.net>
2020-04-20 16:28:55 +0530

    second commit

---
+++ application.py
@@ -0,0 +1,78 @@
+import os
+import datetime
+
+from flask import *
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from models import *
+
+
+app = Flask(__name__)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+@app.route("/")
+def index():
+    return "Project 1: TODO"
+
+
+@app.route("/register", methods= ['POST','GET'])
+def register():
+    Username=""
+    message=""
+    if request.method=='POST':
+        Username=request.form.get("Username")
+        Password=request.form.get("Password")
+        Email=request.form.get("Email")
+        message=Username+" "+"Successfully registered"
+        Timestamp=datetime.datetime.now()
+        users=User(Username,Password,Email,Timestamp)
+        db.add(users)
+        db.commit()
+        return render_template("register.html", message=message)
+    else:
+        return render_template("register.html")
+
+
+@app.route("/admin")
+def admin():
+    data=db.query(User).order_by(User.Timestamp)
+    return render_template('admin.html',list=data)
+
+@app.route("/auth", methods=['POST','GET'])
+def auth(): 
+    if request.method=='POST':
+        Username=request.form.get("Username")
+        Password=request.form.get("Password")
+        user=User.query.get(Username)
+        if (Username==User.Username) and (Password==User.Password):
+            session['User']=user
+            return render_template("account.html")
+        else:
+            return "Please check your credentials"
+
+@app.route("/search")
+def search():
+    if session["Username"]!=None:
+        return "Maintained successfully"
+    else: 
+        return redirect(url_for("/logout"))
+
+@app.route("/logout",methods=['GET','POST'])
+def logout():
+    session["Username"]=None
+    return redirect("/register")
+
